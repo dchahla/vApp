dbd3bfec2ac91555e5a6ad9cb372f84cafbed5ff
Daniel Chahla <doniel@Doniels-MacBook-Pro.local>
2015-02-14 17:51:15 -0600

    Initial

---
+++ www/js/controllers.js
@@ -0,0 +1,17 @@
+angular.module('starter.controllers', [])
+
+.controller('cruiseCtrl', function($scope) {})
+
+.controller('bearCtrl', function($scope) {})
+
+// .controller('bearDetailCtrl', function($scope, $stateParams, bear) {
+//   $scope.bear = bear.get($stateParams.bearId);
+// })
+
+.controller('TripCtrl', function($scope) {})
+
+// .controller('FriendDetailCtrl', function($scope, $stateParams, Trip) {
+//   $scope.friend = Trip.get($stateParams.friendId);
+// })
+
+.controller('concertCtrl', function($scope) {});
